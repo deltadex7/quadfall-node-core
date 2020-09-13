04c013264fa9b191636f385498110529e1703525
Delta Dextra <54788149+deltadex7@users.noreply.github.com>
2020-09-13 12:10:54 +0800

    Added support for documentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   rules: {
     "@typescript-eslint/no-angle-bracket-type-assertion": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
     "prettier/prettier": [
       "error",
       {
